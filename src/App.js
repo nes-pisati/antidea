9877a8f6616e45849c0f12b7970eb27252c0a33c
Vanessa Pisati <pisati.vanessa@gmail.com>
2025-02-09 18:00:21 +0100

    Styles and Routes setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Homepage from './pages/Homepage';
+import Project from './pages/Project';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Homepage />}/>
+        <Route path='/rebels' element={<Project />}/>
+        {/* <Route path='/kilis' element={<Project />}/> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
