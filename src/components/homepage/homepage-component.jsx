8985a6156ed3cd4013087c864aeab4825b276fc2
Vanessa Pisati <pisati.vanessa@gmail.com>
2025-03-26 22:41:21 +0100

    edited Homepage Component

--- src/components/homepage/homepage-component.jsx
+++ src/components/homepage/homepage-component.jsx
@@ -4,7 +4,7 @@ import Container from "../reusable/container/container";
 import TextComponent from "../reusable/text-component/text-component";
 import Gradient from "../reusable/gradient/gradient-component";
 import Projects from "./projects/projects";
-import ChiSiamo from "./chi-siamo/chi-siamo-component";
+import ChiSiamo from "../reusable/chi-siamo/chi-siamo-component";
 import Footer from "../reusable/footer/footer-component";
 
 export default function HomepageComponent() {
@@ -25,7 +25,8 @@ export default function HomepageComponent() {
         <>
             <Hero />
             <Container title={"Ant'dea"}>
-                <TextComponent text={"Antidea. Un’idea prima dell’idea. Lampo. Intuizione. O un’idea contro l’idea. Contro l’idea consueta, contro il luogo comune. O, ancora, davanti alla dea, senza incertezza di fronte a ogni liturgia, a ogni devozione o convenienza. Antidea racconterà storie prima, contro e davanti. Le storie di tutti, semplici, a volte nascoste. Vere. Sempre."} />
+                <TextComponent text={"Antidea. Un’idea prima dell’idea. Lampo. Intuizione. O un’idea contro l’idea. Contro l’idea consueta, contro il luogo comune. O, ancora, davanti alla dea, senza incertezza di fronte a ogni liturgia, a ogni devozione o convenienza. Antidea racconterà storie prima, contro e davanti. Le storie di tutti, semplici, a volte nascoste. Vere. Sempre."} 
+                dimension={"big"}/>
             </Container>
             <Container title={"Progetti"}>
                 <Projects />
