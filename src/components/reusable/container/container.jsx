a540943a171eab1101e56a5cbd4cdea46a74c448
Vanessa Pisati <pisati.vanessa@gmail.com>
2025-03-26 22:40:20 +0100

    Container Component

--- src/components/reusable/container/container.jsx
+++ src/components/reusable/container/container.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import Styles from "./container.module.css"
 
-export default function Container({title, children}) {
+export default function Container({title, children, color}) {
+
+    let colorClass;
+
+    switch (color) {
+        case "red":
+            colorClass = Styles.red;
+            break;
+        case "white":
+            colorClass = Styles.white;
+            break;
+        default:
+            colorClass = Styles.red
+    }
+
     return(
         <div className={Styles.container}>
-            <p className={Styles.title}>{title}</p>
+            <p className={`${Styles.title} ${colorClass}`}>{title}</p>
             {children}
         </div>
     )
