9a5cf9cd2cc422de1d32aba1469fb667dc2169dc
Vanessa Pisati <pisati.vanessa@gmail.com>
2025-03-26 22:40:28 +0100

    Text Component

--- src/components/reusable/text-component/text-component.jsx
+++ src/components/reusable/text-component/text-component.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import Styles from "./text-component.module.css"
 
-export default function TextComponent({ text }) {
+export default function TextComponent({ text, dimension }) {
+
+    let textClass;
+
+    switch (dimension){
+        case "big":
+            textClass = Styles.textBig;
+            break;
+        case "medium":
+            textClass = Styles.textMedium;
+            break;
+        case "small":
+            textClass = Styles.textSmall;
+            break;
+        case "xsmall":
+            textClass = Styles.textXSmall;
+            break;
+        default:
+            textClass = Styles.textMedium;
+    }
+
     return (
         <div>
-            <p className={Styles.text}>
+            <p className={textClass}>
                 {text}
             </p>
         </div>
