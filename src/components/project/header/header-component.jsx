3740cca25c6782dd682439c6ff9f0d8f68d8edbb
Vanessa Pisati <pisati.vanessa@gmail.com>
2025-03-26 22:39:14 +0100

    Header Component

---
+++ src/components/project/header/header-component.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Styles from "./header-component.module.css"
+import Logo from "../../../assets/logo.svg"
+import ProjectInfos from "../../reusable/project-infos/project.infos-component"
+import VideoButton from "../../reusable/video-button/video-button";
+import { Link } from 'react-router-dom';
+
+
+export default function Header({title, description}) {
+    return (
+        <div className={Styles.mainContainer}>
+            <div className={Styles.container}>
+                <Link to={"/"}>
+                    <img src={Logo} alt="Logo Antidea" className={Styles.logo} />
+                </Link>
+                <div className={Styles.containerFooter}>
+                    <div>
+                        <ProjectInfos title={title} description={description} />
+                    </div>
+                    <div className={Styles.btn}>
+                        <VideoButton />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
