0dbf5eff3135f4c33f8c254ff418da6bf14e7603
Vanessa Pisati <pisati.vanessa@gmail.com>
2025-03-26 22:40:52 +0100

    Scroll To Top logic

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Homepage from './pages/Homepage';
 import Project from './pages/Project';
+import ScrollToTop from './components/scroll-to-top/scroll-to-top';
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route index element={<Homepage />}/>
         <Route path='/rebels' element={<Project />}/>
-        {/* <Route path='/kilis' element={<Project />}/> */}
+        <Route path='/project/:projectName' element={<Project />}/>
       </Routes>
     </BrowserRouter>
   );
