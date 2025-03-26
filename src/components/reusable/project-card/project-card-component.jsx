4f3eef7504be876692e7d360e9d3d1bb182e56a5
Vanessa Pisati <pisati.vanessa@gmail.com>
2025-03-26 22:39:04 +0100

    Project Card Component

--- src/components/reusable/project-card/project-card-component.jsx
+++ src/components/reusable/project-card/project-card-component.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import Styles from "./project-card-component.module.css"
+import { Link } from 'react-router-dom';
+import ProjectInfos from "../project-infos/project.infos-component";
 
 export default function ProjectCard({ title, description, background }) {
+
+    const projectName = title.toLowerCase();
+
     return (
-        <div className={Styles.cardContainer}>
-            <img src={background} alt="" className={Styles.image}/>
-            <div className={Styles.textContainer}>
-                <h2 className={Styles.title}>{title}</h2>
-                <p className={Styles.description}>{description}</p>
+        <Link to={`/project/${projectName}`} className={Styles.link}>
+            <div className={Styles.cardContainer} >
+                <img src={background} alt="" className={Styles.image} />
+                <div className={Styles.textContainer}>
+                    <h2 className={Styles.title}>{title}</h2>
+                    <p className={Styles.description}>{description}</p>
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
