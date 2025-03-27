25e10a9efaac2ef8b8c77668076fc0bc4a9f6288
Vanessa Pisati <pisati.vanessa@gmail.com>
2025-03-27 22:56:58 +0100

    Installed PrimeReact

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Homepage from './pages/Homepage';
 import Project from './pages/Project';
 import ScrollToTop from './components/scroll-to-top/scroll-to-top';
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+
+import 'primereact/resources/themes/lara-light-indigo/theme.css'; 
+import 'primereact/resources/primereact.min.css'; 
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <ScrollToTop />
-      <Routes>
-        <Route index element={<Homepage />}/>
-        <Route path='/rebels' element={<Project />}/>
-        <Route path='/project/:projectName' element={<Project />}/>
-      </Routes>
-    </BrowserRouter>
+    <PrimeReactProvider>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path='/rebels' element={<Project />} />
+          <Route path='/project/:projectName' element={<Project />} />
+        </Routes>
+      </BrowserRouter>
+    </PrimeReactProvider>
+
   );
 }
 
