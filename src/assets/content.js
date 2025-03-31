b97721d71614748047eb76d3423de1e9ecd9c665
Vanessa Pisati <pisati.vanessa@gmail.com>
2025-03-31 22:51:01 +0200

    dummy data update

--- src/assets/content.js
+++ src/assets/content.js
@@ -1,24 +1,26 @@
 export const rebels = {
+    bgImage: "https://res.cloudinary.com/dssajrpzl/image/upload/v1740929003/antidea/_BAN6362_vera_verit%C3%A0_h7vfpv.png",
     title: "Rebels",
     location: "Bangladesh",
     subtitle: "Rebels è un documentario che affronta il problema dei matrimoni infantili e delle conseguenti problematiche mediche e sociali che devono affrontare le donne bangladesi.",
     mainText: "In Bangladesh il 17% delle donne è costretta a sposarsi prima dei 15 anni, più del 50% si sposa prima dei 18 anni. L'80% di esse ha già un figlio prima di compiere 20 anni.",
     description: "Rebels è la testimonianza di come questi macro problemi siano superabiligrazie all’istruzione, alla determinazione e alla disobbedienza allo status quodominante. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    imageOne: "",
-    imageTwo: "",
+    imageOne: "https://picsum.photos/600/500",
+    imageTwo: "https://picsum.photos/600/500",
     mission: "Dimostrare che i matrimoni infantili sono superabili attraverso l’istruzione, ladeterminazione e il coraggio. Utilizzare il documentario come strumento per aumentarela consapevolezza e raccogliere fondi mirati, supportando efficacemente le donne e leorganizzazioni che le assistono, e promuovendo un cambiamento sostenibile nellasocietà.",
     vision: "Dimostrare che i matrimoni infantili sono superabili attraverso l’istruzione, ladeterminazione e il coraggio. Utilizzare il documentario come strumento per aumentarela consapevolezza e raccogliere fondi mirati, supportando efficacemente le donne e leorganizzazioni che le assistono, e promuovendo un cambiamento sostenibile nellasocietà.",
     closingText: "Rebels è il racconto della vita di queste donne, di Minuti, oggi ingegnera e docente universitaria, di sua nipote diciasettenne calciatrice della nazionale femminile bengalese di calcio e di tutte le altre ribelli e disobbedienti che ogni giorno combattono con loro."
 }
 
 export const kilis = {
+    bgImage: "https://res.cloudinary.com/dssajrpzl/image/upload/v1743451062/antidea/kilis-card_ysrqtk.png",
     title: "Kilis",
     location: "Siria",
-    subtitle: "Lorem ipsum dolor sit amet",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ullamcorper tellus non nisl volutpat, dignissim tempor magna dignissim. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus imperdiet mollis lectus, quis fringilla enim. In pretium odio a dignissim interdum.",
     mainText: "In Bangladesh il 17% delle donne è costretta a sposarsi prima dei 15 anni, più del 50% si sposa prima dei 18 anni. L'80% di esse ha già un figlio prima di compiere 20 anni.",
     description: "Rebels è la testimonianza di come questi macro problemi siano superabiligrazie all’istruzione, alla determinazione e alla disobbedienza allo status quodominante. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    imageOne: "",
-    imageTwo: "",
+    imageOne: "https://picsum.photos/600/500",
+    imageTwo: "https://picsum.photos/600/500",
     mission: "Dimostrare che i matrimoni infantili sono superabili attraverso l’istruzione, ladeterminazione e il coraggio. Utilizzare il documentario come strumento per aumentarela consapevolezza e raccogliere fondi mirati, supportando efficacemente le donne e leorganizzazioni che le assistono, e promuovendo un cambiamento sostenibile nellasocietà.",
     vision: "Dimostrare che i matrimoni infantili sono superabili attraverso l’istruzione, ladeterminazione e il coraggio. Utilizzare il documentario come strumento per aumentarela consapevolezza e raccogliere fondi mirati, supportando efficacemente le donne e leorganizzazioni che le assistono, e promuovendo un cambiamento sostenibile nellasocietà.",
     closingText: "Rebels è il racconto della vita di queste donne, di Minuti, oggi ingegnera e docente universitaria, di sua nipote diciasettenne calciatrice della nazionale femminile bengalese di calcio e di tutte le altre ribelli e disobbedienti che ogni giorno combattono con loro."
