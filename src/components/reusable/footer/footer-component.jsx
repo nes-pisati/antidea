60872967451d760760f2d218e00a3965f5c6437d
Vanessa Pisati <pisati.vanessa@gmail.com>
2026-05-03 17:24:25 +0200

    Updated footer mail

--- src/components/reusable/footer/footer-component.jsx
+++ src/components/reusable/footer/footer-component.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
         <div className={Styles.container} id="footer">
             <p className={Styles.text}>©Antidea 2025</p>
-            <p className={Styles.text}>antidea.aps@gmail.com</p>
+            <p className={Styles.text}>info@antidea.it</p>
             <p className={Styles.text}>@antidea.aps</p>
         </div>
     )
